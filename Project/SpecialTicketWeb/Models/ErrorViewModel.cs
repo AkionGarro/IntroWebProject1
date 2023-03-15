f17cd94495feb6918625a2cc30fef2f1467962da
AkionGarro <carloscamp1008@estudianTEC.cr>
2023-03-14 20:08:01 -0600

    Initial Configuration Entity FrameWork

---
+++ Project/SpecialTicketWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SpecialTicketWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
