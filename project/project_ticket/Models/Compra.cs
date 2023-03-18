14e6a76eddaf88a2a5a3beca6f873f065668bc41
LuzClara30 <luzclara3011@estudiantec.cr>
2023-03-18 08:17:02 -0600

    cambio de base de datos
    creación de los modelos, controladores, vistas y la identidad sin el add-migration

---
+++ project/project_ticket/Models/Compra.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace project_ticket.Models;
+
+public partial class Compra
+{
+    public int Id { get; set; }
+
+    public int Cantidad { get; set; }
+
+    public DateTime FechaReserva { get; set; }
+
+    public DateTime FechaPago { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public int CreatedBy { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public int UpdatedBy { get; set; }
+
+    public int Active { get; set; }
+
+    public int IdCliente { get; set; }
+
+    public int IdEntrada { get; set; }
+
+    public virtual Entrada IdEntradaNavigation { get; set; } = null!;
+}
