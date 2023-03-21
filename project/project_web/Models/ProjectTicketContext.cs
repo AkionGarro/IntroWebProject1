0ddbbc0c9f6e33798225b1b2eaeba7b66f250305
AkionGarro <carloscamp1008@estudianTEC.cr>
2023-03-20 23:38:12 -0600

    Update

--- project/project_web/Models/ProjectTicketContext.cs
+++ project/project_web/Models/ProjectTicketContext.cs
@@ -32,7 +32,7 @@ public partial class ProjectTicketContext : IdentityDbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySQL("Server=localhost;Port=3306;Database=project_ticket;User=root;");
+        => optionsBuilder.UseMySQL("Server=localhost;Port=3306;Database=specialticket;User=root;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
