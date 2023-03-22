c65f25cb38c85f72bb8317c1443a084927dcae6a
LuzClara30 <luzclara3011@estudiantec.cr>
2023-03-22 14:11:25 -0600

    cambios en escenarios

--- project/project_web/Models/Escenario.cs
+++ project/project_web/Models/Escenario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace project_web.Models;
 
@@ -10,15 +11,15 @@ public partial class Escenario
     public string Nombre { get; set; } = null!;
 
     public string Localizacion { get; set; } = null!;
-
+    [DisplayName("Creado en")]
     public DateTime CreatedAt { get; set; }
-
+    [DisplayName("Creado por")]
     public int CreatedBy { get; set; }
-
+    [DisplayName("Actualizado en")]
     public DateTime UpdatedAt { get; set; }
-
+    [DisplayName("Actualizado por")]
     public int UpdatedBy { get; set; }
-
+    [DisplayName("Activo")]
     public bool Active { get; set; }
 
     public virtual ICollection<Asiento> Asientos { get; } = new List<Asiento>();
