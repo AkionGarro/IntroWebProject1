1e5fefccf23faa09f07d4acdf763960e39dfcc34
AkionGarro <carloscamp1008@estudianTEC.cr>
2023-04-09 23:40:03 -0600

    Update

--- project/project_web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ project/project_web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -116,7 +116,6 @@ namespace project_web.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
-                var roleQuery = _roleManager.Roles.Where(a => a.Name == "User");
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
